61e3836165a6a0e7e2703a453da18737984b49fe
Afiya Madiwale <143885869+AfiyaMadiwale21@users.noreply.github.com>
2025-10-14 11:47:58 +0530

    CodeInput.jsx

--- client/src/components/CodeInput.jsx
+++ client/src/components/CodeInput.jsx
@@ -4,7 +4,7 @@ const CodeInput = ({ code, setCode }) => {
     return (
         <div className='mb-2 md:mb-8'>
             <label className='text-black'>Ask here:</label><br />
-            <textarea className="textarea textarea-xs text-lg border-4 border-gray-400 w-full bg-white text-black" placeholder="type here..." value={code} onChange={(e) => setCode(e.target.value)}></textarea>
+            <textarea className="textarea textarea-xs text-lg border-4 border-gray-200 w-full bg-white text-black" placeholder="type here..." value={code} onChange={(e) => setCode(e.target.value)}></textarea>
 
         </div>
 
