e34d155c0ee30eff28fef67ef0b6539686f361dd
Afiya Madiwale <143885869+AfiyaMadiwale21@users.noreply.github.com>
2025-10-16 16:06:12 +0530

    LoadingSpinner.jsx

--- client/src/components/LoadingSpinner.jsx
+++ client/src/components/LoadingSpinner.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 export const LoadingSpinner = () => {
     return (
-        <div className="px-60 w-full max-w-4xl min-h-[200px] flex flex-col justify-center items-center">
+        <div className="w-full min-h-[200px] flex justify-center items-center">
+            <div className="flex flex-col justify-center items-center">
             <span className="loading loading-spinner loading-xl text-black"></span>
              <span className='text-black mt-3'>please wait</span>
+            </div>
         </div>
     );
 };
